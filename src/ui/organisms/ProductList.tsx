e53788d091d901f80ad374f37fbf3a25fc5290cf
mogiel <mogielski@protonmail.com>
2024-03-05 13:37:24 +0100

    fix types

--- src/ui/organisms/ProductList.tsx
+++ src/ui/organisms/ProductList.tsx
@@ -1,42 +1,48 @@
 import { ProductListItem } from '@/ui/molecules/ProductListItem';
 
+const ProductsListData: Product[] = [
+  {
+    id: '1',
+    name: 'Kot',
+    category: 'Zwierzę',
+    price: 1000,
+    currency: 'PLN',
+    image: 'https://zakopizapomnij.pl/wp-content/uploads/2019/08/Notokoty-Maj-25-%E2%80%94-kopia.jpg',
+    alt: 'p'
+  },
+  {
+    id: '2',
+    name: 'Kot',
+    category: 'Zwierzę',
+    price: 1000,
+    currency: 'PLN',
+    image: 'https://zakopizapomnij.pl/wp-content/uploads/2019/08/Notokoty-Maj-25-%E2%80%94-kopia.jpg',
+    alt: 'p'
+  },
+  {
+    id: '3',
+    name: 'Kot',
+    category: 'Zwierzę',
+    price: 1000,
+    currency: 'PLN',
+    image: 'https://zakopizapomnij.pl/wp-content/uploads/2019/08/Notokoty-Maj-25-%E2%80%94-kopia.jpg',
+    alt: 'p'
+  },
+  {
+    id: '4',
+    name: 'Kot',
+    category: 'Zwierzę',
+    price: 1000,
+    currency: 'PLN',
+    image: 'https://zakopizapomnij.pl/wp-content/uploads/2019/08/Notokoty-Maj-25-%E2%80%94-kopia.jpg',
+    alt: 'p'
+  },
+];
+
 export const ProductList = ({}) => {
   return <ul className={'grid grid-cols-1 gap-8 sm:grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4'}>
-    <ProductListItem
-      product={{
-        description: { product: { name: 'Kot', category: 'Zwierzę', price: 1000, currency: 'PLN' } },
-        image: {
-          image: 'https://zakopizapomnij.pl/wp-content/uploads/2019/08/Notokoty-Maj-25-%E2%80%94-kopia.jpg',
-          alt: 'p'
-        }
-      }}
-    />
-    <ProductListItem
-      product={{
-        description: { product: { name: 'Kot', category: 'Zwierzę', price: 1000, currency: 'PLN' } },
-        image: {
-          image: 'https://zakopizapomnij.pl/wp-content/uploads/2019/08/Notokoty-Maj-25-%E2%80%94-kopia.jpg',
-          alt: 'p'
-        }
-      }}
-    />
-    <ProductListItem
-      product={{
-        description: { product: { name: 'Kot', category: 'Zwierzę', price: 1000, currency: 'PLN' } },
-        image: {
-          image: 'https://zakopizapomnij.pl/wp-content/uploads/2019/08/Notokoty-Maj-25-%E2%80%94-kopia.jpg',
-          alt: 'p'
-        }
-      }}
-    />
-    <ProductListItem
-      product={{
-        description: { product: { name: 'Kot', category: 'Zwierzę', price: 1000, currency: 'PLN' } },
-        image: {
-          image: 'https://zakopizapomnij.pl/wp-content/uploads/2019/08/Notokoty-Maj-25-%E2%80%94-kopia.jpg',
-          alt: 'p'
-        }
-      }}
-    />
+    {
+      ProductsListData.map((product: Product) => <ProductListItem key={product.id} product={product}/>)
+    }
   </ul>;
 };
