15f2c0b160fe9abc8df42bec8c143b3ff20cb633
mogiel <mogielski@protonmail.com>
2024-03-11 16:14:53 +0100

    fix

--- src/app/products/[category]/[pageNumber]/page.tsx
+++ src/app/products/[category]/[pageNumber]/page.tsx
@@ -1,7 +1,7 @@
 export const generateStaticParams = async () => {
   return [{ pageNumber: '1' }, { pageNumber: '2' }];
 };
-export default function ({ params }: { params: { category: string, pageNumber: string, } }) {
+export default function PageCategoryNumber({ params }: { params: { category: string, pageNumber: string, } }) {
   return (
     <h1>
       Produkt z kategorii {params.category}, na stronie {params.pageNumber}.
